5d9a973cc536e8386b41f47c85e348ff0a21c5f4
mikhuov <mikhuovinen@gmail.com>
2023-04-04 18:48:29 +0300

    Changed: Variable

--- Osa3/index.js
+++ Osa3/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-const Person = require('./models/person');
+const Person = require('./models/Person');
 
 app.use(express.json());
 app.use(cors());
