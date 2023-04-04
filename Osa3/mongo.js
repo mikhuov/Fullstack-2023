b433f058fa45e22356820256ed158c7413c6cac6
mikhuov <mikhuovinen@gmail.com>
2023-04-04 18:46:33 +0300

    Changed: Eslint

--- Osa3/mongo.js
+++ Osa3/mongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument');
+    process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
 const url = `mongodb+srv://mikhuovinen:${password}@cluster0.m2jcnla.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -13,31 +13,31 @@ mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: String,
+    number: String
 });
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
 if (process.argv.length === 3) {
     Person.find({}).then(result => {
-      console.log('Phonebook:');
-      result.forEach(person => {
-        console.log(person.name, person.number);
-      });
-      mongoose.connection.close();
+        console.log('Phonebook:');
+        result.forEach(person => {
+            console.log(person.name, person.number);
+        });
+        mongoose.connection.close();
     });
 }
 
 if (process.argv[3] && process.argv[4]) {
 
-  const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
-  });
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    });
 
-  person.save().then(({ name, number }) => {
-    console.log(`Aded ${name} number ${number} to the phonebook!`);
-    mongoose.connection.close();
-  });
+    person.save().then(({ name, number }) => {
+        console.log(`Aded ${name} number ${number} to the phonebook!`);
+        mongoose.connection.close();
+    });
 }
